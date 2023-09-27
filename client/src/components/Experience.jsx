ee54ed6d8be1673c79eaae0ddc136538e36e2c02
Plebeu-Code <astronauta2027@gmail.com>
2023-09-26 23:07:48 -0300

    sistema de movimentação com fisica

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,46 +1,80 @@
 import React, { useState } from "react";
 import * as THREE from "three";
-import { ContactShadows, Environment, OrbitControls, useCursor } from "@react-three/drei";
+import {
+  ContactShadows,
+  Environment,
+  Grid,
+  OrbitControls,
+  useCursor,
+} from "@react-three/drei";
 import { AnimatedWoman } from "./AnimatedWoman";
-import { SocketManager, charactersAtom, socket } from "./SocketManager";
+import {
+  SocketManager,
+  charactersAtom,
+  mapAtom,
+  socket,
+  userAtom,
+} from "./SocketManager";
 import { useAtom } from "jotai";
+import { Item } from "./Item";
+import { useThree } from "@react-three/fiber";
+import { useGrid } from "../hooks/useGrid";
 
 export const Experience = () => {
   const [_characters] = useAtom(charactersAtom);
   const [onFloor, setOnFloor] = useState(false);
-  useCursor(onFloor)
+  const [map] = useAtom(mapAtom);
+  const scene = useThree((state) => state.scene);
+  const [user] = useAtom(userAtom);
 
-  console.log(_characters);
+  useCursor(onFloor);
+
+  const { vector3ToGrid, gridToVector3 } = useGrid();
+
+  const onCharacterMove = (e) => {
+    const character = scene.getObjectByName(`character-${user}`);
+    if (!character) return;
+
+    socket.emit(
+      "move",
+      vector3ToGrid(character.position),
+      vector3ToGrid(e.point)
+    );
+  };
 
   return (
     <>
       <SocketManager />
       <Environment preset="sunset" />
       <ambientLight intensity={0.2} />
-      <ContactShadows blur={3} />
+      {/* <ContactShadows blur={3} />  */}
       <OrbitControls />
+
+      {map?.items.map((item, idx) => (
+        <Item key={`${item.name}-${idx}`} item={item} />
+      ))}
+
       <mesh
         rotation-x={-Math.PI / 2}
-        position-y={-0.001}
-        onClick={(e) => socket.emit("move", [e.point.x, 0, (e.point.z)])}
+        position-y={-0.002}
+        onClick={onCharacterMove}
         onPointerEnter={() => setOnFloor(true)}
         onPointerLeave={() => setOnFloor(false)}
+        position-x={map?.size[0] / 2}
+        position-z={map?.size[1] / 2}
       >
-        <planeGeometry args={[20, 20]} />
+        <planeGeometry args={map?.size} />
         <meshStandardMaterial color="#f0f0f0" />
       </mesh>
+      <Grid infiniteGrid fadeDistance={50} fadeStrength={5} />
       {_characters.length > 0
         ? _characters.map((character) => (
             <AnimatedWoman
               key={character.id}
+              id={character.id}
+              path={character.path}
               hairColor={character.hairColor}
-              position={
-                new THREE.Vector3(
-                  character.position[0],
-                  character.position[1],
-                  character.position[2]
-                )
-              }
+              position={gridToVector3(character.position)}
               topColor={character.topColor}
               bottomColor={character.bottomColor}
             />
