47897fbc90dd4b3841bcf6bb90d62b4e8ade8b42
Plebeu-Code <astronauta2027@gmail.com>
2023-09-26 23:08:23 -0300

    nova movimentação

--- client/src/components/SocketManager.jsx
+++ client/src/components/SocketManager.jsx
@@ -4,9 +4,13 @@ import { useAtom, atom } from "jotai"
 
 export const socket = io("http://localhost:3001")
 export const charactersAtom = atom([])
+export const mapAtom = atom(null)
+export const userAtom = atom(null)
 
 export const SocketManager = () => {
   const [_characters, setCharacters] = useAtom(charactersAtom)
+  const [map, setMap] = useAtom(mapAtom)
+  const [user, setUser] = useAtom(userAtom)
 
   useEffect(() => {
     function onConnect() {
@@ -17,8 +21,21 @@ export const SocketManager = () => {
       console.log("Desconectado");
     }
 
-    function onMessage() {
-      console.log("Messagem");
+    function onMessage(value) {
+      setMap(value.map)
+      setCharacters(value.characters)
+      setUser(value.id)
+    }
+
+    function onPlayerMove(value) {
+      setCharacters((prev) => {
+        return prev.map(character => {
+          if (character.id === value.id) {
+            return value
+          }
+          return character
+        })
+      })
     }
 
     function onCharacters(value) {
@@ -29,12 +46,14 @@ export const SocketManager = () => {
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
     socket.on("message", onMessage);
+    socket.on("playerMove", onPlayerMove);
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
       socket.off("message", onMessage);
       socket.off("characters", onCharacters);
+      socket.off("playerMove", onPlayerMove);
     }
   })
 }
