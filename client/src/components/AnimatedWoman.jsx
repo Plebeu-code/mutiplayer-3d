9704d0ea27714a88b84514a9cb9a6de4e2b64edd
Plebeu-Code <astronauta2027@gmail.com>
2023-09-26 23:07:21 -0300

    novo sistema de movimentação

--- client/src/components/AnimatedWoman.jsx
+++ client/src/components/AnimatedWoman.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { useFrame, useGraph } from "@react-three/fiber";
 import { SkeletonUtils } from "three-stdlib";
+import { useAtom } from "jotai";
+import { userAtom } from "./SocketManager";
+import { useGrid } from "../hooks/useGrid";
 
 const MOVEMENT_SPEED = 0.032;
 
@@ -9,48 +12,70 @@ export function AnimatedWoman({
   hairColor = "green",
   topColor = "pink",
   bottomColor = "brown",
+  id,
   ...props
 }) {
-  const position = useMemo(() => props.position, []);
 
+  const position = useMemo(() => props.position, []);
+  const [path, setPath] = useState()
+  const { gridToVector3 } = useGrid()
   const group = useRef();
 
   const { scene, materials, animations } = useGLTF(
     "/models/Animated Woman.glb"
   );
 
-  // Malhas skinned não podem ser reutilizadas em Threejs sem cloná-las; esta é uma solução alternativa.
   const clone = useMemo(() => SkeletonUtils.clone(scene), [scene]);
-
   const { nodes } = useGraph(clone);
-
   const { actions } = useAnimations(animations, group);
-
   const [animation, setAnimation] = useState("CharacterArmature|Idle");
 
   useEffect(() => {
-    actions[animation].reset().fadeIn().play();
-    return () => actions[animation]?.fadeOut(0.5);
-  });
+      const path = []
+      props.path?.forEach(gridPosition => {
+          path.push(gridToVector3(gridPosition))
+      });
+      setPath(path)
+  }, [props.path])
 
-  useFrame(() => {
-    if (group.current.position.distanceTo(props.position) > 0.1) {
+  useEffect(() => {
+    actions[animation].reset().fadeIn(0.32).play();
+    return () => actions[animation]?.fadeOut(0.32);
+  }, [animation]);
+
+  const [user] = useAtom(userAtom);
+
+  useFrame((state) => {
+    if (path?.length && group.current.position.distanceTo(path[0]) > 0.1) {
       const direction = group.current.position
         .clone()
-        .sub(props.position)
+        .sub(path[0])
         .normalize()
         .multiplyScalar(MOVEMENT_SPEED);
-
-        group.current.position.sub(direction)
-        group.current.lookAt(props.position)
-        setAnimation("CharacterArmature|Run")
+      group.current.position.sub(direction);
+      group.current.lookAt(path[0]);
+      setAnimation("CharacterArmature|Run");
+    } else if (path?.length) {
+      path.shift()
     } else {
-      setAnimation("CharacterArmature|Idle")
+      setAnimation("CharacterArmature|Idle");
+    }
+    if (id === user) {
+      state.camera.position.x = group.current.position.x + 8;
+      state.camera.position.y = group.current.position.y + 8;
+      state.camera.position.z = group.current.position.z + 8;
+      state.camera.lookAt(group.current.position);
     }
   });
 
   return (
-    <group ref={group} {...props} position={position} dispose={null}>
+    <group
+      ref={group}
+      {...props}
+      position={position}
+      dispose={null}
+      name={`character-${id}`}
+    >
       <group name="Root_Scene">
         <group name="RootNode">
           <group
